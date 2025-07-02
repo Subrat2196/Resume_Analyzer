bce6ffa16c94c9cda8e5d40dea369063804b06bb
subrat bahuguna <bahuguna.subrat211996@gmail.com>
2025-07-02 14:07:51 +0530

    logger

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,11 +1,12 @@
 import os
 
+# it is to save the analysis of your project
 def save_text_to_file(text, output_path):
     """Save text to a file"""
     with open(output_path,'w',encoding='utf-8') as file:
         file.write(text)
     
-
+# it will remove the already existing file if is present 
 def remove_file(file_path):
     """Remove a file if it exists"""
     if os.path.exists(file_path):
